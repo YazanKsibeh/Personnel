b5a5a8016dff88d41162a3f72c0c53c176a7315e
Yazan-Ksibeh <yazantkas@gmail.com>
2023-02-11 00:38:30 +0300

    This is the second commit.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Hello there</div>;
+  return (
+    <div className="App">
+      <div>Hello there</div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
